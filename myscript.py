ac5d1a96b97b96524e57cfc4df5439dbf60051be
63070132-song-ngan-kub-jan <63070132@it.kmitl.ac.th>
2023-02-28 16:05:55 +0700

    Fixed jenkins_slave

--- myscript.py
+++ myscript.py
@@ -1,12 +1,25 @@
-import argparse
+with open('./name.txt', encoding='utf-8') as name_file:
+    name = name_file.read()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("name", help="the name to greet")
-parser.add_argument("--age", help="the age of the person being greeted", type=int)
-args = parser.parse_args()
+with open('./age.txt', encoding='utf-8') as age_file:
+    age = age_file.read()
 
-greeting = "Hello, " + args.name + "!"
-if args.age:
-    greeting += " You are " + str(args.age) + " years old."
+greeting = "Hello, " + name + "!"
+
+if age:
+    greeting += " You are " + age + " years old."
 
 print(greeting)
+
+# import argparse
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument("name", help="the name to greet")
+# parser.add_argument("--age", help="the age of the person being greeted", type=int)
+# args = parser.parse_args()
+
+# greeting = "Hello, " + args.name + "!"
+# if args.age:
+#     greeting += " You are " + str(args.age) + " years old."
+
+# print(greeting)
